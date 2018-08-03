bb9ec9099f42a50acb5179f29d174442006b0340
xiaowei <shenzhoudance@gmail.com>
2018-08-03 19:17:38 +0800

    edit welcome idnex

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :messages do
     resources :comments
   end
-  root 'messages#index'
+  root 'welcome#index'
 end
